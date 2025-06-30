ac4dbd0e8471019b71b7ea49f06ea5be766653ed
identityofsine <identityofsine@gmail.com>
2025-06-30 00:12:28 -0400

    very basic setup as of now

--- src/app/core/routes/app.routes.ts
+++ src/app/core/routes/app.routes.ts
@@ -16,5 +16,14 @@ export const routes: Routes = [
       title: 'Login',
       breadcrumb: 'Login'
     }
+  },
+  {
+    path: 'app',
+    loadComponent: () => import('@pli-feature/home').then(m => m.HomeComponent),
+  },
+  //TODO: probably want to move this route to the home lib - just because the home page could be different based on the user's role
+  {
+    path: '',
+    loadComponent: () => import('@pli-feature/home').then(m => m.HomeComponent),
   }
 ];
