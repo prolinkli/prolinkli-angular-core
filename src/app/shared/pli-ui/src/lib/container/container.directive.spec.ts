2236330c48f85d169495593e01b3fb460ff8cac9
kevin <identityofsine@gmail.com>
2025-07-02 20:48:45 -0400

    test

--- src/app/shared/pli-ui/src/lib/container/container.directive.spec.ts
+++ src/app/shared/pli-ui/src/lib/container/container.directive.spec.ts
@@ -1,8 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { PliContainer } from './container.directive';
 
+@Component({
+  template: '<div pli-container>Test content</div>',
+  imports: [PliContainer]
+})
+class TestComponent {}
+
 describe('PliContainer', () => {
-  it('should create an instance', () => {
-    const directive = new PliContainer();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let directiveElement: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    directiveElement = fixture.debugElement.query(By.directive(PliContainer));
+  });
+
+  it('should create the directive', () => {
+    expect(directiveElement).toBeTruthy();
+  });
+
+  it('should add pli-container class on init', () => {
+    fixture.detectChanges();
+    const element = directiveElement.nativeElement;
+    expect(element.classList.contains('pli-container')).toBeTruthy();
   });
 });
